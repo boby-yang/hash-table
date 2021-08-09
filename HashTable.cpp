ce5a15024796606657e9920abc95421df98df851
Bob Yang <boby.sfu.ca>
2021-08-09 12:41:43 +0800

    Finished all functions, need testing/print stats done for testing purpose

--- HashTable.cpp
+++ HashTable.cpp
@@ -8,46 +8,192 @@
  * sets cur_size to 0
  * sets P2, the value used by h2
  */
-HashTable()
+HashTable::HashTable()
 {
 	capacity = 101;
 	cur_size = 0;
-	p2 = next_prime(capacity);
-	data = new string(capacity);
+	sec_hash_const = next_prime(capacity / 2);
+	data = new string[capacity];
 	for (int i = 0; i < capacity; ++i)
 	{
 		data[i] = "";
 	}
 }
 
-HashTable(int n)
+/*
+ * Constructor of the HashTable
+ * Param: number of strings to store
+ * Post: creates a hash table to store n items;
+ * capacity should be the smallest prime >= 2n
+ * sets all array elements to the empty string ("");
+ * sets cur_size to 0
+ * sets P2, the value used by h2
+ */
+HashTable::HashTable(int n)
 {
-
+	capacity = next_prime(2 * n - 1);
+	cur_size = 0;
+	sec_hash_const = next_prime(capacity / 2);
+	data = new string[capacity];
+	for (int i = 0; i < capacity; ++i)
+	{
+		data[i] = "";
+	}
 }
 
-HashTable(const HashTable &src)
+/*
+ * Copy constructor of the HashTable
+ * Param: constant reference of a HashTable object
+ * Post: creates a deep copy of the HashTable parameter;
+ */
+HashTable::HashTable(const HashTable &src)
 {
-
+	capacity = src.capacity;
+	cur_size = src.cur_size;
+	sec_hash_const = src.sec_hash_const;
+	data = new string[capacity];
+	for (int i = 0; i < capacity; ++i)
+	{
+		data[i] = src.data[i];
+	}
 }
 
-~HashTable()
+/*
+ * Destructor of the QueueT
+ * Param: none
+ * Post: table of strings will be deleted
+ */
+HashTable::~HashTable()
 {
-
+	delete[] data;
 }
 
-
-HashTable operator=(const HashTable &src)
+/*
+ * Overridden assignment operation of the HashTable
+ * Param: constant reference of a HashTable object
+ * Post:(parameter != this) all strings in the HashTable will deleted;
+ * 		cur_size will be set to parameter's cur_size;
+ * 		capacity will be set to parameter's capacity;
+ * 		all string in parameter will be copied in order
+ */
+HashTable HashTable::operator=(const HashTable &src)
 {
+	if (&src != (const HashTable *) this)
+	{
+		if (nullptr != data)
+		{
+			delete[] data;
+		}		
+		capacity = src.capacity;
+		cur_size = src.cur_size;
+		sec_hash_const = src.sec_hash_const;
+		data = new string[capacity];
+		for (int i = 0; i < capacity; ++i)
+		{
+			data[i] = src.data[i];
+		}
+	}
 	return *this;
 }
 
-void insert(string data)
+
+/*
+ * Pre: HashTable obj need to be created
+ * Param: string to insert to table
+ * Post: searches the hashtable for the parameter, 
+ * if not found, inserts the param into the hash table;
+ * then check load factor, if lf > 0.67 after insertion:
+ * (1) create new hashtable of size first prime > 2 * original size
+ * (2) inserts the original contents in the new table at the appropriate places
+ * (3) sets the value used by h2
+ */
+void HashTable::insert(string str)
 {
 
+
+	cout << "Insert "  << str << endl;
+
+
+	if (false == find(str))
+	{
+		int cur_index = get_hash_index(str, capacity);
+		int h2_gap = get_hash_index(str, sec_hash_const);
+		while ("" != data[cur_index])
+		{
+
+
+			cout << "Looking at index:" << to_string(cur_index) << endl;
+
+
+			cur_index = (cur_index + h2_gap) % capacity;
+		}
+		data[cur_index] = str;
+		cur_size++;
+
+
+		cout << "Insert " << str << " at index:" << to_string(cur_index) << endl;
+		print_table_stats();
+		cout << endl;
+
+
+		if (loadFactor() > 0.67)
+		{
+
+			cout << "Table needs expansion" << endl;
+
+			int new_size = next_prime(2 * capacity);
+			string *new_data = new string[new_size];
+			string *tmp = data;
+			data = new_data;
+			capacity = new_size;
+			sec_hash_const = next_prime(capacity / 2);
+			cur_size = 0;
+
+
+			cout << "Before migration:" << endl;
+			print_table_stats();
+			cout << endl;
+
+
+			for (int i = 0; i < capacity; ++i)
+			{
+				insert(tmp[i]);
+			}
+
+
+
+			cout << "After migration:" << endl;
+			print_table_stats();
+			cout << endl;
+
+
+			delete[] tmp; // need to test for valgrind
+		}
+	}
 }
 
-bool find(string target)
+/*
+ * Pre: HashTable obj need to be created
+ * Param: string for target to look for
+ * Post: returns true if its string parameter is in the hash table
+ * returns false if it is not
+ */
+bool HashTable::find(string target)
 {
+	if ("" == target)
+	{
+		return false;
+	}
+	int cur_index = get_hash_index(target, capacity);
+	int h2_gap = get_hash_index(target, sec_hash_const);
+	while ("" != data[cur_index])
+	{
+		if (target == data[cur_index])
+		{
+			return true;
+		}
+		cur_index = (cur_index + h2_gap) % capacity;
+	}
 	return false;
 }
 
@@ -56,7 +202,7 @@ bool find(string target)
  * Param: none
  * Post: returns number of strings in the HashTable
  */
-int size()
+int HashTable::size()
 {
 	return cur_size;
 }
@@ -66,7 +212,7 @@ int size()
  * Param: none
  * Post: returns capacity of the HashTable
  */
-int maxSize()
+int HashTable::maxSize()
 {
 	return capacity;
 }
@@ -76,7 +222,7 @@ int maxSize()
  * Param: none
  * Post: returns load factor of the HashTable
  */
-double loadFactor()
+double HashTable::loadFactor()
 {
 	return (double)cur_size / capacity;
 }
@@ -89,7 +235,7 @@ double loadFactor()
  * Post: returns true if param is prime
  * false otherwise
  */
-bool is_prime(int num)
+bool HashTable::is_prime(int num)
 {
 	for (int i = 2; i * i <= num; ++i)
 	{
@@ -103,11 +249,11 @@ bool is_prime(int num)
 
 /*
  * Pre: none
- * Param: integer
+ * Param: an integer
  * Post: returns the smallest prime number
  * greater than the parameter
  */
-int next_prime(int num)
+int HashTable::next_prime(int num)
 {
 	for (int i = num + 1; ;++i)
 	{
@@ -118,3 +264,44 @@ int next_prime(int num)
 	}
 	return -1; // should never reach here
 }
+
+/*
+ * Pre: none
+ * Param: string to calculate the string value
+ * Post: returns the index of hash function using the equation:
+ * ch0 * 32^n-1 + ch1 * 32^(n-2) + ... + ch(n-2) * 32^1 + ch(n-1) * 32^0
+ * and we use honer's method to avoid overflowing
+ */
+int HashTable::get_hash_index(string str, int mod_val)
+{	
+	if (0 == str.size())
+	{
+		return 0;
+	}
+	int out = 0;
+	for (int i = 0; i < str.size(); ++i)
+	{
+		int ascii = str[i] - 96;
+		out = (32 * out + ascii) % mod_val;
+	}
+	return out;
+}
+
+
+
+///// for testing purpose
+void HashTable::print_table_stats()
+{
+	cout << "Table stats:" << endl;
+	cout << "Size:" << to_string(cur_size) << endl;
+	cout << "Capacity:" << to_string(capacity) << endl;
+	cout << "P2:" << to_string(sec_hash_const) << endl;
+	cout << "Data:" << endl;
+	for (int i = 0; i < capacity; ++i)
+	{
+		if ("" != data[i])
+			cout << to_string(i) << ":" << data[i] << " ";
+	}
+	cout << endl;
+}
+/////
--- HashTable.h
+++ HashTable.h
@@ -1,14 +1,21 @@
 #include <string>
+using namespace std;
+
+
+///// for testing purpose
+#include <iostream>
+/////
 
 class HashTable
 {
 private:
 	string *data;
 	int capacity;
-	int p2;
+	int sec_hash_const;
 	int cur_size;
 	bool is_prime(int num);
 	int next_prime(int num);
+	int get_hash_index(string str, int mod_val);
 
 public:
 	HashTable();
@@ -16,9 +23,13 @@ public:
 	HashTable(const HashTable &src);
 	~HashTable();
 	HashTable operator=(const HashTable &src);
-	void insert(string data);
+	void insert(string strs);
 	bool find(string target);
 	int size();
 	int maxSize();
 	double loadFactor();
-}
+	
+	///// for testing purpose
+	void print_table_stats();
+	/////
+};
